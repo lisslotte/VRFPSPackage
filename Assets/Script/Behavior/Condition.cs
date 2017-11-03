c82bfa261ae7657080219a8dd8af0e260f4e546c
lisslotte <30319456+lisslotte@users.noreply.github.com>
2017-11-03 19:57:50 +0800

    .6

---
+++ Assets/Script/Behavior/Condition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Behavior {
+    public class Condition : MonoBehaviour
+    {
+
+        protected virtual bool CanSeeObject(GameObject obj, float seeAngel,float seeDistance)
+        {
+            if (Vector3.Distance(transform.position, obj.transform.position) < seeDistance)
+            {
+                return true;
+            }
+            return false;
+        }
+        protected virtual bool CanAttackObject(GameObject obj,float attackDistance)
+        {
+            if (Vector3.Distance(transform.position, obj.transform.position) < attackDistance)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/Script/Behavior/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Behavior {
+    public abstract class Movement : MonoBehaviour {
+        protected abstract void OnSeekWalk(GameObject obj);
+        protected abstract void OnSeekRun(GameObject obj);
+        protected abstract void OnAttack(GameObject obj);
+        protected abstract void OnWait();
+        protected abstract void OnWander(float radius, float delayTime);
+        protected abstract void OnDead();
+        protected abstract void OnDamage();
+    }
+}
